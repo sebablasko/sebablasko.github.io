048e54bbf233ff117bf3875f1df7adf5d11f08a2
Sebastian Blasco <sebablasko@gmail.com>
2021-06-02 21:07:13 -0400

    update wallet

---
+++ wallet/precache-manifest.69780e2d7b41249900e0e18679935391.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c0e434e69cc6e2c00a4",
+    "url": "/wallet/css/app.b7c53b6e.css"
+  },
+  {
+    "revision": "0af823bf46e5086c03e1c71956104eaf",
+    "url": "/wallet/index.html"
+  },
+  {
+    "revision": "0c0e434e69cc6e2c00a4",
+    "url": "/wallet/js/app.0ec1752e.js"
+  },
+  {
+    "revision": "fad70fde07edb2238158",
+    "url": "/wallet/js/chunk-vendors.f6888de1.js"
+  },
+  {
+    "revision": "87caf51c64178e1840b64b57110189f8",
+    "url": "/wallet/manifest.json"
+  },
+  {
+    "revision": "525076bd704a7a43b1298cfd221d65c6",
+    "url": "/wallet/media/beep.525076bd.mp3"
+  }
+]);
